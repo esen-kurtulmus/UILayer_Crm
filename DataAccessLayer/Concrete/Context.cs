5d0f11bffbaa38409a09507204da0e58243a40b0
esenkurt <esenkurt@LAPTOP-IQULJ22J>
2022-09-20 22:16:40 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-IQULJ22J\\SQLEXPRESS;database=DbCrm;integrated security=True;");
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
